4c33c24012f357b84c384b3dfd172fddf1ca8ffe
hosseinvejdani <vejdani.shojae@gmail.com>
2021-09-28 22:55:01 +0330

    first setting up in ubuntu

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+# 
+#only in ubuntu vvv
+import pymysql
+pymysql.install_as_MySQLdb()
+#only in ubuntu ^^^
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
