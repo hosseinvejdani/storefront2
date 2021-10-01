37eb769e9a05446a763ddcf0deee9720376f0392
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-01 19:40:30 +0330

    2.13- Deserializing Objects. adding POST support to product_list method in very simple way.

--- store/views.py
+++ store/views.py
@@ -8,13 +8,16 @@ from .serializers import ProductSerializer
 
 # Create your views here.
 
-@api_view()
+@api_view(['GET','POST'])
 def product_list(request):
-    # use select_related for nested format too.
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset,many=True,context={'request': request})
-    return Response(serializer.data)
-
+    if request.method == 'GET':
+        # use select_related for nested format too.
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset,many=True,context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(request.data)
+        return Response('ok')
 
 @api_view()
 def product_details(request,id):
