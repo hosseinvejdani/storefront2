e71579df5335663959dcf87cadac096e25c66dca
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-01 09:26:49 +0330

    2.9 - Serializing Objects in views.py  : prevent error for "product does not exists" - mathod 2 -> as better way

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -15,10 +15,6 @@ def product_list(request):
 
 @api_view()
 def product_details(request,id):
-    try:
-        product = Product.objects.get(pk = id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        # return Response(status=404)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product,pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
