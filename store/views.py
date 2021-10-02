811bda46314b849c57d2279077bfd96eeeb4f00e
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-02 13:49:03 +0330

    2.3 and 2.4 - Mixins :  mixin is a class which ensapsulate some pattern for us. Generic Views containes different mixins inside it and can be used in projects easily - way 1

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,18 +10,16 @@ from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-class ProductList(APIView):
-    def get(self,request):
-        # use select_related for nested format too.
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset,many=True,context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
 
-    def post(self,request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    def get_serializer_class(self):
+        return ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
 
 
 
