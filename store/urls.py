838b2e34ee65fcdaaa6f084a6aa022d3b32fe6eb
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-02 14:39:24 +0330

    2.3 and 2.4 - Mixins :  mixin is a class which ensapsulate some pattern for us. Generic Views containes different mixins inside it and can be used in projects easily - way 2 - if you dont need apply some logic when handling requests, you can try this way. 2.5 - customizing generic views.

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
-    path('products/<int:id>', views.ProductDetails.as_view()),
-    path('collections/', views.collection_list),
-    path('collections/<int:pk>', views.collection_details)
+    path('products/<int:pk>', views.ProductDetails.as_view()),
+    path('collections/', views.CollectionList.as_view()),
+    path('collections/<int:pk>', views.CollectionDetails.as_view())
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from django.http import HttpResponse
-from rest_framework.generics import ListCreateAPIView 
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,35 +11,35 @@ from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
 class ProductList(ListCreateAPIView):
-    # in this way you can apply some logic for each method
-    # if you dont need to apply logic, you can use way 2 : next commit
-    def get_queryset(self):
-        return Product.objects.select_related('collection').all()
-
-    def get_serializer_class(self):
-        return ProductSerializer
+    #  if you dont need apply some logic when handling requests, you can try this way.
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
 
+    # if you need apply some logic when handling requests, you can try this way.
+    # def get_queryset(self):
+    #     return Product.objects.select_related('collection').all()
+
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+
+    # def get_serializer_context(self):
+    #     return {'request': self.request}
 
 
 
-class ProductDetails(APIView):
-    def get(self,request,id):
-        product = get_object_or_404(Product,pk=id)
-        serializer = ProductSerializer(product,context={'request': request})
-        return Response(serializer.data)
 
-    def put(self,request,id):
-        product = get_object_or_404(Product,pk=id)
-        serializer = ProductSerializer(product,data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+class ProductDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all() # !!!!!!!!!! id??
+    serializer_class = ProductSerializer
 
-    def delete(self,request,id):
-        product = get_object_or_404(Product,pk=id)
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def delete(self,request,pk):
+        product = get_object_or_404(Product,pk=pk)
         if product.orderitem_set.count()>0:
             return Response({'error':'method can not be applied becouse there are one or more order item associated with this product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
@@ -47,42 +47,27 @@ class ProductDetails(APIView):
 
 
 
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('product')).all()
+    serializer_class = CollectionSerializer
 
 
+class CollectionDetails(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.annotate(products_count = Count('product')).all()
+    serializer_class = CollectionSerializer
 
-
-
-@api_view(['GET','POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(products_count=Count('product')).all()
-        serializer = CollectionSerializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return  Response(serializer.data,status=status.HTTP_201_CREATED)
-
-@api_view(['GET','PUT','DELETE'])
-def collection_details(request,pk):
-    collection = get_object_or_404(
-        Collection.objects.annotate(
-            products_count = Count('product')
-        ),
-        pk=pk
-    )
-    if request.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer =  CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    elif request.method == 'DELETE':
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection,pk=pk)
         if collection.product_set.count()>0:
             msg = 'this collection can not be del;etd becouse it containes one or more products in it'
             return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    
+
+
+
+
+
