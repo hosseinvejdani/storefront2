e1382514903963b085f755708af98aec88d54aaa
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-02 12:55:36 +0330

    17- Exercise- Building the Collections API

--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
 
@@ -38,6 +39,39 @@ def product_details(request,id):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view()
+
+
+@api_view(['GET','POST'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.annotate(products_count=Count('product')).all()
+        serializer = CollectionSerializer(queryset,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return  Response(serializer.data,status=status.HTTP_201_CREATED)
+
+@api_view(['GET','PUT','DELETE'])
 def collection_details(request,pk):
-    return  Response('ok')
+    collection = get_object_or_404(
+        Collection.objects.annotate(
+            products_count = Count('product')
+        ),
+        pk=pk
+    )
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer =  CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        if collection.product_set.count()>0:
+            msg = 'this collection can not be del;etd becouse it containes one or more products in it'
+            return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
