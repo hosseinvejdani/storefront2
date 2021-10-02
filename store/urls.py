de47b76bb27a1d7a88b80b0bea67e15b5c0c82c8
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-02 13:28:45 +0330

    3.2- Class-based Views - convert function base views to class base views.

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('products/', views.product_list),
-    path('products/<int:id>', views.product_details),
+    path('products/', views.ProductList.as_view()),
+    path('products/<int:id>', views.ProductDetails.as_view()),
     path('collections/', views.collection_list),
     path('collections/<int:pk>', views.collection_details)
 ]
