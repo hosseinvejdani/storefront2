019436bfc9525f6faf785794cf028ef906cb4a9b
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-05 08:58:58 +0330

    2.6 - define ViewSet - 2.7 - add router

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet) 
 
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>', views.ProductDetails.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>', views.CollectionDetails.as_view())
-]
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>', views.ProductDetails.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('collections/<int:pk>', views.CollectionDetails.as_view())
+# ]
--- store/views.py
+++ store/views.py
@@ -5,66 +5,56 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
-# Create your views here.
-class ProductList(ListCreateAPIView):
-    #  if you dont need apply some logic when handling requests, you can try this way.
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-    # if you need apply some logic when handling requests, you can try this way.
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collection').all()
+    # def destroy(self,request,pk):
+    #     product = get_object_or_404(Product,pk=pk)
+    #     if product.orderitem_set.count()>0:
+    #         return Response({'error':'method can not be applied becouse there are one or more order item associated with this product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     product.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
-    # def get_serializer_class(self):
-    #     return ProductSerializer
+    # ====  OR Try This =====================
 
-    # def get_serializer_context(self):
-    #     return {'request': self.request}
-
-
-
-
-class ProductDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all() # !!!!!!!!!! id??
-    serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        return {'request': self.request}
-
-    def delete(self,request,pk):
-        product = get_object_or_404(Product,pk=pk)
+    def destroy(self, request,*args,**kwargs):
+        # product = get_object_or_404(Product,pk=pk)  # this is magical part of django
         if product.orderitem_set.count()>0:
             return Response({'error':'method can not be applied becouse there are one or more order item associated with this product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request,*args,**kwargs)
 
 
-
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
+    # def destroy(self, request, pk):
+    #     collection = get_object_or_404(Collection,pk=pk)
+    #     if collection.product_set.count()>0:
+    #         msg = 'this collection can not be del;etd becouse it containes one or more products in it'
+    #         return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    #     collection.delete()
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
-class CollectionDetails(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count = Count('product')).all()
-    serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection,pk=pk)
+    # ====  OR Try This =====================
+
+    def destroy(self, request,*args,**kwargs):
+        # collection = get_object_or_404(Collection,pk=kwargs['pk']) # this is magical part of django
         if collection.product_set.count()>0:
             msg = 'this collection can not be del;etd becouse it containes one or more products in it'
             return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
+        return super().destroy(request,*args,**kwargs)
     
 
 
