6afbbb66f4e37488de080dd355c9f8c146cc3e53
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-05 09:01:25 +0330

    end of 2.7 - clean code

--- store/views.py
+++ store/views.py
@@ -11,24 +11,13 @@ from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 class ProductViewSet(ModelViewSet):
-
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
 
-    # def destroy(self,request,pk):
-    #     product = get_object_or_404(Product,pk=pk)
-    #     if product.orderitem_set.count()>0:
-    #         return Response({'error':'method can not be applied becouse there are one or more order item associated with this product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    #     product.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
-    # ====  OR Try This =====================
-
     def destroy(self, request,*args,**kwargs):
-        # product = get_object_or_404(Product,pk=pk)  # this is magical part of django
         if product.orderitem_set.count()>0:
             return Response({'error':'method can not be applied becouse there are one or more order item associated with this product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request,*args,**kwargs)
@@ -38,21 +27,9 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
-    # def destroy(self, request, pk):
-    #     collection = get_object_or_404(Collection,pk=pk)
-    #     if collection.product_set.count()>0:
-    #         msg = 'this collection can not be del;etd becouse it containes one or more products in it'
-    #         return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    #     collection.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-    # ====  OR Try This =====================
-
     def destroy(self, request,*args,**kwargs):
-        # collection = get_object_or_404(Collection,pk=kwargs['pk']) # this is magical part of django
         if collection.product_set.count()>0:
-            msg = 'this collection can not be del;etd becouse it containes one or more products in it'
+            msg = 'this collection can not be delete becouse it containes one or more products in it'
             return Response({'error':msg},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request,*args,**kwargs)
     
