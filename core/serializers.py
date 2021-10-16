01e4f8f42891717c6eb96315350b8bbc55986079
hosseinvejdani <vejdani.shojae@gmail.com>
2021-10-16 21:53:37 +0330

    6.9 - get current user

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,9 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer,UserCreateSerializer as BaseUserCreateSerializer
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id','first_name','last_name','username','password','email']
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id','username','email','first_name','last_name']
